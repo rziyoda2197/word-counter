3be7bd23d48047346ab0b6fbaddd933f02dac057
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-20 23:47:47 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,5 @@
+def sozlar_soni(text):
+    return len(text.split())
+
+text = "So'zlar sonini hisoblang. Faqat va faqat tayyor, optimal va ishlashga qodir toza kodni bering."
+print(sozlar_soni(text))
